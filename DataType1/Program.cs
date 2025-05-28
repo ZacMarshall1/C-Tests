61bbe14cdc38a2455ebf177a34c217e35d05c500
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-05-28 19:41:48 -0300

    data types examples and their values

---
+++ DataType1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Assigning numeric variables");
+Console.WriteLine();
+//Assigning variables
+byte value1 = 255;
+sbyte value2 = -127;
+int value3 = -2147483647;
+uint value4 = 2147483647;
+long value5 = -21474836489;
+ulong value6 = 21474836489;
+
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+Console.WriteLine(value3);
+Console.WriteLine(value4);
+Console.WriteLine(value5);
+Console.WriteLine(value6);
+                          
