725c160320b42fd5d86e00b0042d6c7d9eb1cc64
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-05-29 16:36:20 -0300

    Testing bool and char values

---
+++ DataType3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Assigning values to bool & char");
+
+bool active = true;
+System.Boolean unactive = false;
+
+Console.WriteLine(active);
+Console.WriteLine(unactive);
+
+
+Console.WriteLine(10 == 15);
+Console.WriteLine(10 == 10);
+
+int x = 10;
+int y = 15;
+Console.WriteLine(x > y);
+
+char letter1 = 'A';
+char letter2 = '\u0041';
+
+Console.WriteLine(letter1);
+Console.WriteLine(letter2);
+
+Console.ReadLine();
