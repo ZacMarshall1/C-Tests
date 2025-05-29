58d48312ee6d8a8e41e45816b9196fb94c8c3554
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-05-29 15:44:08 -0300

    testing floating point numbers

---
+++ DataType2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Atribbuting values to double, float and decimal");
+Console.WriteLine();
+
+double n1 = 1.234;
+float n2 = 1.234f;
+decimal n3 = 1.234m;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+
+Console.ReadLine();
+
+Console.WriteLine("Comparing float, double and decimal's precision");
+
+float x = 1f / 3f;
+double y = 1d / 3d;
+decimal z = 1m / 3m;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.ReadLine();
