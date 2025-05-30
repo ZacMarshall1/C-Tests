bf08405e778bc0d28e322e3e77eca4c6fc6a4ed5
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-05-30 17:12:32 -0300

    Formatting data outputs with escape sequence

---
+++ DataOutput2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("data output using escape sequence");
+Console.WriteLine();
+
+string local = "c:\\test\\cuGames.txt";
+string sentence = "He said :\"is wasn't me\"";
+string pizza = "\nPizza\nMozzarella";
+string cake = "\nChocolate\tcake";
+
+Console.WriteLine(local);
+Console.WriteLine(sentence);
+Console.WriteLine(pizza);
+Console.WriteLine(cake);
