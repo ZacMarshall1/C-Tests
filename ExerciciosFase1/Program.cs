8c8a17b1c57412bd2e577a1ffbfdaecb5baa5c40
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-05-30 16:14:35 -0300

    Exercises for testing knowledge

---
+++ ExerciciosFase1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Exercises1");
+
+int age = 35;
+string name = "Maria";
+float height = 3.45f;
+DateTime Date = new DateTime(1999, 09, 04);
+const int YEAR = 12;
+double? grade = 7.80;
+
+Console.WriteLine(age);
+Console.WriteLine(name);
+Console.WriteLine(height);
+Console.WriteLine(Date);
+Console.WriteLine(YEAR);
+Console.WriteLine(grade);
+
+int x = 10;
+double number = 7.99;
+char letter = 'C';
+float temperature = 27.4f;
+bool active = false;
+string name1 = "Manoel";
+decimal wage = 950.99m;
+DateTime today = DateTime.Now;
+
+int x1 = 77;
+int y1 = 66;
+int z = x1 + y1;
+Console.WriteLine(z);
