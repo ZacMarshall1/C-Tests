c9f2da58ca0e6c2455e00dad49ac67836fa28998
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-05-30 16:26:47 -0300

    Formatting data outputs

---
+++ DataOutput1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Data output Formatting: ");
+
+int age = 25;
+string name = "Maria";
+//Concatenation
+Console.WriteLine(name + " is " + age + " years old");
+//interpolation
+Console.WriteLine($"{name} is {age} years old");
+//placeHolders
+Console.WriteLine("{0} is {1} years old", name , age);
+
+Console.ReadLine();
