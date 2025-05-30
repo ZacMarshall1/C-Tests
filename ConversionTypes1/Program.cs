7928761edc3795359e7c311436557c48cc0438f6
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-05-30 17:59:58 -0300

    testing converting different number values

---
+++ ConversionTypes1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("## Conversion Types ##\n");
+
+int varInt = 100;
+double varDouble = varInt;
+
+Console.WriteLine(varDouble);
+
+int intNumber = 2145678;
+long longNumber = intNumber;
+float floatNumber = intNumber;
+double doubleNumber = intNumber;
+decimal numberDecimal = intNumber;
+
+Console.WriteLine(intNumber);
+Console.WriteLine(longNumber);
+Console.WriteLine(floatNumber);
+Console.WriteLine(doubleNumber);
+Console.WriteLine(numberDecimal);
+
+Console.ReadLine();
