ae3f1f433afb9345d95c12360a194ba522609f39
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-02 18:57:51 -0300

    Experimenting basic data input operations

---
+++ DataInput1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("\n ## Data Input ##");
+
+Console.WriteLine("\nEnter your name");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("\nEnter your age");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\nYour name is {name}, and your are {age} years old.");
