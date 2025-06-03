fba95614d4dd3aeb01ef3c38f3726a0678e02bbd
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-03 18:42:24 -0300

    Testing with logical operators

---
+++ LogicalOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("## Logical Operators ##");
+
+bool c1 = 5 >= 7;
+bool c2 = 9 != 8;
+bool result;
+
+Console.WriteLine($"c1 = {c1}");
+Console.WriteLine($"c2 = {c2}");
+
+result = c1 && c2;
+Console.WriteLine($"And Operator Result {result}");
+
+result = c1 || c2;
+Console.WriteLine($"Or Operator Result {result}");
+
+result = !(c1 && c2);
+Console.WriteLine($"Not Operator Result {result}");
