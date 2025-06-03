5cfd56ae33ded8957dd7204cdd6b4fb8031209ea
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-03 17:58:45 -0300

    Testing with constants and Math class

---
+++ ConstTest/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(" ## Constants ##");
+
+const int YEAR = 12;
+
+const int MONTH = 30, WEEK = 7, FORTNIGHT = 14;
+
+const int MONTHS_YEAR = 12;
+const int DAYS_YEAR = 365;
+
+const float DAYS_PER_MONTH = (float)DAYS_YEAR / (float)MONTHS_YEAR;
+
+double radius = Convert.ToDouble(Console.ReadLine());
+double perimeter;
+double area;
+
+const double PI = Math.PI;
+
+perimeter = PI * 2 * radius;
+area = PI * Math.Pow(radius, 2);
+
+Console.WriteLine($"perimiter = {perimeter}, area = {area}");
