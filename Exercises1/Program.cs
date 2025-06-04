fd2042e5a8290d0a56ab82fa4f447a3d6198dfe2
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-04 18:26:35 -0300

    exercises in CS

---
+++ Exercises1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("## Exercises 1 ##");
+
+string name = "Paul";
+int age = 17;
+float grade = 7.5f;
+
+Console.WriteLine($"Student:{name}, is {age} years old and a score of {grade}");
+
