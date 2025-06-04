2519140904a4c4a117a1abdfbbba1180deb5a35e
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-04 17:34:44 -0300

    Testing with unary and ternary operators

---
+++ UnaryAndTernaryOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("## Unary And Ternary Operators ##");
+
+Console.WriteLine("Informe um número: \n");
+
+int n = Convert.ToInt32(Console.ReadLine());
+string? greaterOrLower = n > 50 ? "is greater than " : 
+                         n < 50 ? "is lower than " : 
+                         n == 50 ? "is equal to " : "No Valid Answer";
+
+Console.WriteLine($"The negative version of {n} is : {-n}");
+
+Console.WriteLine($"{n} {greaterOrLower}50");
