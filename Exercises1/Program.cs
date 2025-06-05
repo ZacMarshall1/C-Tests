185f45a96e2eda52f49deb24d59f4563add85542
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-05 18:12:06 -0300

    exercises in CS

--- Exercises1/Program.cs
+++ Exercises1/Program.cs
@@ -5,4 +5,29 @@ int age = 17;
 float grade = 7.5f;
 
 Console.WriteLine($"Student:{name}, is {age} years old and a score of {grade}");
+Console.WriteLine($"{name}\n{age}\n{grade}");
+
+string? myInput = Console.ReadLine();
+
+char letterOne = myInput[0];
+
+myInput = Console.ReadLine();
+
+char letterTwo = myInput[0];
+
+myInput = Console.ReadLine();
+
+char letterThree = myInput[0];
+
+myInput = null;
+
+Console.WriteLine($"Letter three: {letterThree}\nletter two: {letterTwo}\nletter one: {letterOne}");
+
+double value1 = Convert.ToDouble(Console.ReadLine());
+double value2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Sum: {value1 + value2}");
+Console.WriteLine($"Subtraction: {value1 - value2}");
+Console.WriteLine($"Division: {value1 / value2}");
+Console.WriteLine($"Multiplication: {value1 * value2}");
 
--- Exercises1/obj/Debug/net9.0/Exercises1.AssemblyInfo.cs
+++ Exercises1/obj/Debug/net9.0/Exercises1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercises1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2519140904a4c4a117a1abdfbbba1180deb5a35e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2042e5a8290d0a56ab82fa4f447a3d6198dfe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercises1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercises1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
