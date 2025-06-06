0c1d7cf306ef757492902c0d5bca8d20548c73d3
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-06 18:22:28 -0300

    exercise making a functional bhaskara's formula calculator

---
+++ BhaskarasFormulaCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("## Bhaskara's formula ##");
+
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+
+double delta = Math.Pow(b, 2) - 4 * a * c;
+
+if (delta < 0)
+{
+    Console.WriteLine("No real roots (delta is negative).");
+}
+else
+{
+    double resultOne = (-b + Math.Sqrt(delta)) / (2 * a);
+    double resultTwo = (-b - Math.Sqrt(delta)) / (2 * a);
+
+    Console.WriteLine($"Delta = {delta}");
+    Console.WriteLine($"Root 1: {resultOne}");
+    Console.WriteLine($"Root 2: {resultTwo}");
+}
