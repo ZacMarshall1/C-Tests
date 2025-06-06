2097a90daedcbf048166c9aecb2e43e9e791d142
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-06 18:50:30 -0300

    Login Exercise using ternary operators

---
+++ LoginExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("## Login Test ##");
+
+string userOne = "admin";
+string userTwo = "maria";
+string password = "123";
+
+Console.WriteLine("Enter your user: ");
+string selectedUser = Console.ReadLine();
+Console.WriteLine("Enter your password");
+string selectedPassword = Console.ReadLine();
+
+
+string result = ((selectedUser == userOne || selectedUser == userTwo) && selectedPassword == password)
+    ? "Login Successful" : "Unable to log in";
+
+Console.WriteLine(result);
+
