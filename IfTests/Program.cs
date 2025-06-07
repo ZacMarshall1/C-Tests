bdd23ec878191607cebd130e61ef5f789dae2641
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-07 12:16:41 -0300

    Testing if statements

---
+++ IfTests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("## If statement ##");
+
+Console.WriteLine("Special client (true/false)");
+bool isSpecial = Convert.ToBoolean(Console.ReadLine());
+
+if(isSpecial)
+{
+    Console.WriteLine("10% discount");
+}
+Console.WriteLine("End of program");
+
+Console.WriteLine("Enter an x value: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter an y value:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("X is greater than Y");
+}
+else if(x == y)
+{
+    Console.WriteLine("X is equal to Y");
+}
+else
+{
+    Console.WriteLine("Y is greater than X");
+}
