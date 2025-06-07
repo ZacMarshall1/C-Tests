2ffe8857d90725e7ff82a7aee74aa6434092f728
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-06 23:37:16 -0300

    Translate temperature from celcius to farhenheit and kelvin

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("## Temperature Converter ##");
+
+Console.WriteLine("Enter a temperature in celcius");
+
+double celcius = Convert.ToDouble(Console.ReadLine());
+double kelvin = celcius + 273;
+double farhenheit = (celcius * 9) / 5 + 32;
+
+Console.WriteLine($"Celcius temperature: {celcius}");
+Console.WriteLine($"Kelvin temperature: {kelvin}");
+Console.WriteLine($"Farhenheit temperature: {farhenheit}");
+
