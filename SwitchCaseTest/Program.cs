7682fe6c727d47362ff8ea0deb5a08bc58e01254
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-07 16:31:15 -0300

    testing with switch case statement

---
+++ SwitchCaseTest/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("## Switch Case Test ##");
+
+double price = 600.00;
+
+Console.WriteLine($"Purchase Value: ${price}");
+Console.WriteLine("Enter the number of installments (1 to 3):");
+int installmentNumber = Convert.ToInt32(Console.ReadLine());
+
+switch (installmentNumber)
+{
+    case 1:
+        Console.WriteLine("Single payment.");
+        break;
+
+    case 2:
+    case 3:
+        double installmentValue = price / installmentNumber;
+        Console.WriteLine($"{installmentNumber} installment(s) of ${installmentValue:F2} each.");
+        break;
+
+    default:
+        Console.WriteLine("Invalid payment method.");
+        break;
+}
