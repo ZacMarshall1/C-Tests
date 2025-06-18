5cb31f31eed546bd7be4cb46d8dddfb321080017
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-06-18 10:32:17 -0300

    while loop test

---
+++ RepetitionStructuresTest/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Repetition Structures ##");
+
+//goto (not recommended)
+int i = 1;
+double input  = Convert.ToDouble(Console.ReadLine());
+
+//repeat:
+
+//    Console.WriteLine($"i= {i}");
+
+//    i++;
+
+//if (i < 10)
+//    goto repeat;
+
+while (i <= 10)
+{
+    double result = input * i;
+    Console.WriteLine($"{input} * {i} = {result}");
+    i++;
+}
+
+Console.WriteLine("End of process...");
